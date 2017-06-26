c978b41a0c78ba875e36a7376581e3f9956a0494
M Homam <mharjomandi@yahoo.com>
2017-06-26 13:37:00 -0400

    TEST DEV PROD ENV

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+
+// process.env.NODE_ENV === 'test'
+// process.env.NODE_ENV === 'production'
+// process.env.NODE_ENV === 'development'
