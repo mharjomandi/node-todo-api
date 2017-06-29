5930675d795406f4dce244910192aeb9e1682882
M Homam <mharjomandi@yahoo.com>
2017-06-28 23:57:17 -0400

    Add tests for user login

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -89,7 +99,6 @@ UserSchema.statics.findByCredentials = function (email, password) {
     });
   });
 };
-
 
 UserSchema.pre('save', function (next) {
   var user = this;
