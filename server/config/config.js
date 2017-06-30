987b557fd92b1ac84b76468aa39510b2a245ef07
M Homam <mharjomandi@yahoo.com>
2017-06-30 00:05:54 -0400

    Add config variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,10 @@
-var env = process.env.NODE_ENV || 'devellpment';
-                                //This environment variable dows not exist on
-                                //normal machine, in Heroku it exists
-                                //to add this variable in do that in package
+var env = process.env.NODE_ENV || 'development';
 
-console.log ('env ***********', env);
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
 
-if (env === 'devellpment'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
